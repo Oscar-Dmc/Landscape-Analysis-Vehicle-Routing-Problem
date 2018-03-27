6f7115455a01ae6c9f29c0e1933021833f9fd059
Óscar <alu0100705638@ull.edu.es>
2018-03-27 11:39:06 +0100

    Desarrollo de la clase Node

---
+++ TFG-VRP/src/main/java/oscardmc/vrp/Node.java
@@ -0,0 +1,27 @@
+package main.java.oscardmc.vrp;
+
+public class Node {
+
+	private int x; 
+	private int y; 
+	private String id; 
+	/*private static int demand;*/
+	
+	public Node(int x, int y, String id){
+		this.x = x;
+		this.y = y;
+		this.id = id;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	public String getId(){
+		return this.id; 
+	} 
+}
