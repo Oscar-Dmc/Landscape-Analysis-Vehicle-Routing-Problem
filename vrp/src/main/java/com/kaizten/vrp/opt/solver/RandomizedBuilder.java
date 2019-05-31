8e3147ce034b937d2aaef28c927d3fb6453e5526
alu0100705638 <alu0100705638@ull.edu.es>
2019-05-31 18:12:16 +0100

    Add new builder and update on libaries

--- vrp/src/main/java/com/kaizten/vrp/opt/solver/RandomizedBuilder.java
+++ vrp/src/main/java/com/kaizten/vrp/opt/solver/RandomizedBuilder.java
@@ -7,104 +7,36 @@ import com.kaizten.opt.solver.Solver;
 import com.kaizten.vrp.opt.core.Vrp;
 
 public class RandomizedBuilder implements Solver<RoutesSolution<Vrp>>{
+
+	private Vrp problem; 
+	private RoutesSolution<Vrp> solution; 
+	private ArrayList<Integer> customers; 
+	private ArrayList<Integer> routes; 
 	
-	private Vrp problem;
-	private RoutesSolution<Vrp> solutionVrp; 
-	private boolean[] cl;
-	private ArrayList<Integer> rcl;
-	private int rclSize;
-	
-	
-	public RandomizedBuilder(Vrp problem) {
-		this.problem = problem;
-		this.solutionVrp = new RoutesSolution<Vrp>(problem, problem.getNCustomers(), problem.getNVehicles());
-		this.rcl =  new ArrayList<Integer>();
-		this.cl = new boolean[problem.getNCustomers()];
-		for(int i = 0;  i < this.cl.length;  i++) {
-			this.cl[i] = true; 
+	public RandomizedBuilder (Vrp problem) {
+		this.problem = problem; 
+		for(int i = 0; i < problem.getNCustomers(); i++) {
+			this.customers.add(i);
 		}
-		this.rclSize = problem.getNMaxCustomers();
-	}
-	
-	private void ConstructRandomizedSolution() {
-		int currentRoute = 0;
-		while (!this.solutionVrp.isFull()) {
-			MakeRCL(currentRoute);
-			int indexCustomer = SelectElementAtRandom();
-
-			if (this.solutionVrp.getLengthRoute(currentRoute) == this.problem.getNMaxCustomers()) {
-				currentRoute++;
-			}
-
-			if (this.solutionVrp.isEmpty(currentRoute)) {
-				this.solutionVrp.addAfterDepot(indexCustomer, currentRoute);
-			} else {
-				this.solutionVrp.addAfter(indexCustomer, this.solutionVrp.getLastInRoute(currentRoute));
-			}
+		for(int i = 0; i < problem.getNVehicles(); i++){
+			this.routes.add(i);
 		}
+		this.solution =  new RoutesSolution<Vrp>(problem, problem.getNCustomers(), problem.getNVehicles());
 	}
-
-	private void MakeRCL(int route) {
-		if (this.solutionVrp.isEmpty(route)) {
-			while (this.rcl.size() < this.rclSize && CandidatesAvailable()) {
-				double minDistance = Double.MAX_VALUE;
-				int indCustomer = -1;
-				for (int i = 0; i < this.cl.length; i++) {
-					if (this.problem.getDistanceMatrix()[0][i + 1] < minDistance
-							&& cl[i]) { /* In the distance matrix 0 is for the depot */
-						indCustomer = i;
-						minDistance = this.problem.getDistanceMatrix()[0][i + 1];
-					}
-				}
-				this.rcl.add(indCustomer);
-				this.cl[indCustomer] = false;
-			}
-		} else {
-			while (this.rcl.size() < this.rclSize && CandidatesAvailable()) {
-				double minDistance = Double.MAX_VALUE;
-				int indCustomer = -1;
-				for (int i = 0; i < this.cl.length; i++) {
-					int lastCustomer = solutionVrp.getLastInRoute(route);
-					if (this.problem.getDistanceMatrix()[lastCustomer + 1][i + 1] < minDistance && cl[i]) {
-						indCustomer = i;
-						minDistance = this.problem.getDistanceMatrix()[lastCustomer + 1][i + 1];
-					}
-				}
-				this.rcl.add(indCustomer);
-				this.cl[indCustomer] = false;
-			}
-		}
-	}
-
-	private int SelectElementAtRandom() {
-		int randomCustomer = -1;
-		if (this.rcl.size() == 1) {
-			randomCustomer = this.rcl.get(0);
-			this.rcl.remove(0);
-		} else {
-			int randomIndex = (int) (Math.random() * this.rcl.size());
-			randomCustomer = this.rcl.get(randomIndex);
-			this.rcl.remove(randomIndex);
-		}
-		return randomCustomer;
-	}
-
-	private boolean CandidatesAvailable() {
-		for (int i = 0; i < this.cl.length; i++) {
-			if (this.cl[i]) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 	@Override
 	public RoutesSolution<Vrp> run() {
-		this.ConstructRandomizedSolution();
-		this.solutionVrp.evaluate();
-		
-		return this.solutionVrp;
+		int indexCustomer = -1;
+		int indexRoute = -1;
+		while(solution.getNumberOfNonIncluded() != 0) {
+			indexCustomer =  (int) (Math.random() * this.customers.size());
+			indexRoute = (int) (Math.random() * this.routes.size());
+			this.solution.addInRoute(indexRoute, indexCustomer);
+			this.customers.remove(indexCustomer);
+			if(this.solution.getLengthRoute(indexRoute) == this.problem.getNMaxCustomers()) {
+				this.routes.remove(indexRoute);
+			}
+		}
+		return this.solution;
 	}
-	
 
 }
--- vrp/src/main/java/com/kaizten/vrp/opt/solver/Vns.java
+++ vrp/src/main/java/com/kaizten/vrp/opt/solver/Vns.java
@@ -1,15 +1,28 @@
 package com.kaizten.vrp.opt.solver;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.kaizten.opt.evaluator.Evaluator;
 import com.kaizten.opt.move.Move;
+import com.kaizten.opt.move.MoveRoutesSolutionInsertionAfter;
+import com.kaizten.opt.move.MoveRoutesSolutionInsertionBefore;
 import com.kaizten.opt.move.MoveRoutesSolutionMoveAfter;
 import com.kaizten.opt.move.MoveRoutesSolutionMoveBefore;
 import com.kaizten.opt.move.MoveRoutesSolutionSwap;
+import com.kaizten.opt.move.MoveRoutesSolutionRemove;
 import com.kaizten.opt.move.acceptor.MoveAcceptor;
-import com.kaizten.opt.move.acceptor.MoveAcceptorFirstImprovement;
+import com.kaizten.opt.move.acceptor.MoveAcceptorBestImprovement;
+//import com.kaizten.opt.move.acceptor.MoveAcceptorFirstImprovement;
 import com.kaizten.opt.move.applier.Applier;
 import com.kaizten.opt.move.applier.MoveApplier;
+import com.kaizten.opt.move.applier.MoveApplierRoutesSolutionInsertionAfter;
+import com.kaizten.opt.move.applier.MoveApplierRoutesSolutionInsertionBefore;
+import com.kaizten.opt.move.applier.MoveApplierRoutesSolutionRemove;
 import com.kaizten.opt.move.explorer.MoveExplorer;
 import com.kaizten.opt.move.explorer.MoveExplorerBasic;
+import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionInsertionAfter;
+import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionInsertionBefore;
 import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionMoveAfter;
 import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionMoveBefore;
 import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionSwap;
@@ -20,101 +33,149 @@ import com.kaizten.vrp.opt.core.Vrp;
 import com.kaizten.vrp.opt.move.applier.MoveApplierRoutesSolutionMoveAfter;
 import com.kaizten.vrp.opt.move.applier.MoveApplierRoutesSolutionMoveBefore;
 import com.kaizten.vrp.opt.move.applier.MoveApplierRoutesSolutionSwap;
+import com.kaizten.vrp.opt.move.generator.MoveGeneratorRoutesSolutionRemove;
 
 
 public class Vns implements Solver<RoutesSolution<Vrp>>{
 
 	private RoutesSolution<Vrp> originalSolution;
 	private RoutesSolution<Vrp> auxSolution;
+	private RoutesSolution<Vrp> incompleteSolution; 
+	private Random randomGenerator; 
 	private MoveManagerSequential<RoutesSolution<Vrp>, ?> manager; 
 	private MoveGeneratorRoutesSolutionSwap<RoutesSolution<Vrp>, MoveRoutesSolutionSwap> mGSwap; 
 	private MoveGeneratorRoutesSolutionMoveAfter<RoutesSolution<Vrp>, MoveRoutesSolutionMoveAfter> mGAfter;
 	private MoveGeneratorRoutesSolutionMoveBefore<RoutesSolution<Vrp>, MoveRoutesSolutionMoveBefore> mGBefore;
+	private MoveGeneratorRoutesSolutionRemove<RoutesSolution<Vrp>, MoveRoutesSolutionRemove> mGRemove; 
+	private MoveGeneratorRoutesSolutionInsertionAfter<RoutesSolution<Vrp>, MoveRoutesSolutionInsertionAfter> mGInsertionAfter;
+	private MoveGeneratorRoutesSolutionInsertionBefore<RoutesSolution<Vrp>, MoveRoutesSolutionInsertionBefore> mGInserionBefore; 
 	private Applier<RoutesSolution<Vrp>> gApplier; 
-	private MoveAcceptor acceptor; 
-	private MoveExplorer explorer; 
-	private double executionTime; 
+	private MoveAcceptor acceptor;
+	//private MoveAcceptor random; 
+	private MoveExplorer explorer;
+	private ArrayList<Integer> neighborhoods; 
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public Vns(Vrp problem) {
-		SequentialBuilder builder  = new SequentialBuilder(problem);
-		this.originalSolution =  builder.run();
+	public Vns(Vrp problem, int construct) {
+		if(construct == 1) {
+			SequentialBuilder builder  = new SequentialBuilder(problem);
+			this.originalSolution = builder.run();
+		} else if(construct == 2) {
+			RandomizedBuilder builder =  new RandomizedBuilder(problem); 
+			this.originalSolution = builder.run();
+		} else {
+			RandomizedRCLBuilder builder =  new RandomizedRCLBuilder(problem); 
+			this.originalSolution =  builder.run();
+		}
+		this.randomGenerator = new Random();
+		
+		System.out.println("Initial solution");
+		System.out.println(this.originalSolution);
 		
 		this.manager = new MoveManagerSequential();
 		this.manager.setSolution(this.originalSolution);
 		this.mGSwap = new MoveGeneratorRoutesSolutionSwap<RoutesSolution<Vrp>, MoveRoutesSolutionSwap>();
 		this.mGAfter = new MoveGeneratorRoutesSolutionMoveAfter<RoutesSolution<Vrp>, MoveRoutesSolutionMoveAfter>();
 		this.mGBefore = new MoveGeneratorRoutesSolutionMoveBefore<RoutesSolution<Vrp>, MoveRoutesSolutionMoveBefore>();
+		this.mGRemove = new  MoveGeneratorRoutesSolutionRemove<RoutesSolution<Vrp>, MoveRoutesSolutionRemove>();
+		this.mGInsertionAfter = new MoveGeneratorRoutesSolutionInsertionAfter<RoutesSolution<Vrp>, MoveRoutesSolutionInsertionAfter>();
+		this.mGInserionBefore = new MoveGeneratorRoutesSolutionInsertionBefore<RoutesSolution<Vrp>, MoveRoutesSolutionInsertionBefore>();
 		this.manager.addMoveGenerator(this.mGSwap);
 		this.manager.addMoveGenerator(this.mGAfter);
 		this.manager.addMoveGenerator(this.mGBefore);
+		this.manager.addMoveGenerator(this.mGRemove);
+		this.manager.addMoveGenerator(this.mGInsertionAfter);
+		this.manager.addMoveGenerator(this.mGInserionBefore);
 		this.manager.init();
 		
 		this.gApplier =  new Applier<RoutesSolution<Vrp>>();
 		MoveApplier applierSwap =  new MoveApplierRoutesSolutionSwap();
 		MoveApplier applierMoveAfter = new MoveApplierRoutesSolutionMoveAfter();
 		MoveApplier applierMoveBefore = new MoveApplierRoutesSolutionMoveBefore();
+		MoveApplier applierRemove = new MoveApplierRoutesSolutionRemove();
+		MoveApplier applierInsertionAfter = new MoveApplierRoutesSolutionInsertionAfter();
+		MoveApplier applierInsertionBefore = new MoveApplierRoutesSolutionInsertionBefore();
 		this.gApplier.addMoveApplier(applierSwap);
 		this.gApplier.addMoveApplier(applierMoveAfter);
 		this.gApplier.addMoveApplier(applierMoveBefore);
+		this.gApplier.addMoveApplier(applierRemove);
+		this.gApplier.addMoveApplier(applierInsertionAfter);
+		this.gApplier.addMoveApplier(applierInsertionBefore);
 		
-		this.acceptor = new MoveAcceptorFirstImprovement();
+		//this.acceptor = new MoveAcceptorFirstImprovement();
+		this.acceptor = new MoveAcceptorBestImprovement();
+		//this.random = new MoveAcceptorRandom();
 		this.explorer = new MoveExplorerBasic();
 		
-		this.executionTime = 60;
-	
 	}
 	
 	public RoutesSolution<Vrp> shake(int environment){
-		this.manager.activeExclusively(environment);
-		this.auxSolution =  this.originalSolution.clone();
+		this.manager.activeExclusively(this.neighborhoods.get(environment));
+		
+		if(this.neighborhoods.get(environment) > 3) {
+			this.auxSolution = this.incompleteSolution.clone();
+		} else {			
+			this.auxSolution =  this.originalSolution.clone();
+		}
+		ArrayList<Move> availableMoves =  this.getAvailableMoves();
+
+		int indexRandom = this.randomGenerator.nextInt(availableMoves.size());
+		Move move = availableMoves.get(indexRandom);
+		//System.out.println("Move: " + move + " Index Random: " + indexRandom);
+		
 		this.gApplier.setSolution(this.auxSolution);
-		this.gApplier.setMove(this.manager.next());
-		return this.gApplier.apply(); 
+		this.gApplier.setMove(move);
+		this.gApplier.apply();
+	
+		return this.auxSolution; 
 	}
 	
-	public RoutesSolution<Vrp> firstImprovement(RoutesSolution<Vrp> solution) {
-		RoutesSolution<Vrp> bestSolution = solution; 
+	public RoutesSolution<Vrp> improvement(RoutesSolution<Vrp> solution) {
+		RoutesSolution<Vrp> bestSolution = solution.clone(); 
 		this.explorer.setMoveManager(this.manager);
 		this.explorer.setMoveAcceptor(this.acceptor);
-		boolean improvement = true; 
-		while(improvement) {
-			improvement = false;
-			this.explorer.setSolution(solution);
-			Move acceptedMove = this.explorer.explore();
-			if(acceptedMove != null) {
-				this.gApplier.setSolution(solution);
-				this.gApplier.setMove(acceptedMove);
-				bestSolution =  this.gApplier.apply();
-				improvement =  true; 
-				this.acceptor.init();
-			}
+		this.explorer.setSolution(bestSolution);
+		Move acceptedMove =  this.explorer.explore();
+		if(acceptedMove != null) {
+			this.gApplier.setSolution(bestSolution);
+			this.gApplier.setMove(acceptedMove);
+			bestSolution =  this.gApplier.apply();
 		}
 		return bestSolution;
 	}
 	
 
-	public void neighborhoodChange(RoutesSolution<Vrp> neighborSolution, int k){
-		if (this.originalSolution.getObjectiveFunctionValue(0) >= neighborSolution.getObjectiveFunctionValue(0)) {	
-			this.setOriginalSolution(neighborSolution);
-			k = 0;
+	public int neighborhoodChange(RoutesSolution<Vrp> neighborSolution, int k){
+		if(neighborSolution.getNumberOfNonIncluded() == 0) {
+			if (this.originalSolution.getObjectiveFunctionValue(0) > neighborSolution.getObjectiveFunctionValue(0)) {
+				this.setOriginalSolution(neighborSolution);
+				k = 0;
+			} else {
+				k++;
+			}
 		}
-		else {
-			k++;
+		else{
+			if(this.neighborhoods.get(k) == 3) {
+				this.setIncompleteSolution(neighborSolution);
+				k++;
+			} else if (this.originalSolution.getObjectiveFunctionValue(0) > neighborSolution.getObjectiveFunctionValue(0)) {
+				this.setIncompleteSolution(neighborSolution);
+			} else {
+				this.manager.init();
+			}
 		}
+		return k; 
 	}
 	
 	public RoutesSolution<Vrp> basicVns(int kMax) {
-		while (this.executionTime > 0) {
-
-			long tInit =  System.currentTimeMillis();
-			for(int k = 0;  k < kMax; k++) {
-				this.neighborhoodChange(this.firstImprovement(this.shake(k)), k);;
-			}
-
-			long tEnd =  System.currentTimeMillis();
-			this.executionTime  -= ((tEnd - tInit) * 0.001);
+		int k = 0;
+		while (k < kMax) {
+			RoutesSolution<Vrp> rShake = this.shake(k);
+			RoutesSolution<Vrp> rImprovement = this.improvement(rShake);
+			k = this.neighborhoodChange(rImprovement, k);
+			//System.out.println("El valor de la k  es: " + k);
 		}
+	
 		return this.originalSolution;
 	}
 	
@@ -122,31 +183,42 @@ public class Vns implements Solver<RoutesSolution<Vrp>>{
 	public RoutesSolution<Vrp> run() {
 		this.originalSolution.evaluate();
 		
-		return basicVns(this.manager.getNumberOfMoveGenerators());
+		return basicVns(this.neighborhoods.size());
 	} 
 	
 	/* ------ Gets and sets ---- */ 
 	public RoutesSolution<Vrp> getOriginalSolution() {
 		return originalSolution;
 	}
+	
+	public ArrayList<Move> getAvailableMoves(){
+		//this.manager.init();
+		ArrayList<Move> availableMoves =  new ArrayList<Move>();
+		while(this.manager.hasNext()) {
+			Move move =  this.manager.next();
+			if(move.getDeviationObjectiveFunctionValue(0) != -Evaluator.OBJECTIVE_INFEASIBLE) {
+				availableMoves.add(move);
+			}
+		}
+		this.manager.init();
+		//availableMoves.forEach((n) -> System.out.println(n));
+		return availableMoves; 
+	}
 
 	public void setOriginalSolution(RoutesSolution<Vrp> solution) {
-		this.originalSolution = solution;
-		this.manager.removeMoveGenerator(this.mGSwap);
-		this.manager.removeMoveGenerator(this.mGAfter);
-		this.manager.removeMoveGenerator(this.mGBefore);
+		this.originalSolution = solution.clone();
 		this.manager.setSolution(this.originalSolution);
-		this.manager.addMoveGenerator(this.mGSwap);
-		this.manager.addMoveGenerator(this.mGAfter);
-		this.manager.addMoveGenerator(this.mGBefore);
-		this.mGSwap.init();
-		this.mGAfter.init();
-		this.mGBefore.init();
+		this.manager.init();
 	}
 	
-	public void setExecutionTime(int tMax) {
-		this.executionTime =  tMax; 
+	public void setIncompleteSolution(RoutesSolution<Vrp> solution) {
+		this.incompleteSolution = solution.clone();
+		this.manager.setSolution(this.incompleteSolution);
+		this.manager.init();
 	}
-
+	
+	public void setNeighborhood(ArrayList<Integer> neighborhoods) {
+		this.neighborhoods = neighborhoods; 
+	}
 	
 }
