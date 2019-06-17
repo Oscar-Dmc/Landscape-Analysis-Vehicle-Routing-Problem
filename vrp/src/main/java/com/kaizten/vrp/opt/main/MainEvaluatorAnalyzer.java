40da7a74a3053325bc689d900a017813e5ef2e11
alu0100705638 <alu0100705638@ull.edu.es>
2019-06-17 16:05:15 +0100

    add new compare and Evaluators for moves

--- vrp/src/main/java/com/kaizten/vrp/opt/main/MainEvaluatorAnalyzer.java
+++ vrp/src/main/java/com/kaizten/vrp/opt/main/MainEvaluatorAnalyzer.java
@@ -1,27 +1,50 @@
 package com.kaizten.vrp.opt.main;
 
 import com.kaizten.opt.evaluator.EvaluatorAnalyzer;
+import com.kaizten.opt.move.MoveRoutesSolutionInsertionAfter;
+import com.kaizten.opt.move.MoveRoutesSolutionInsertionBefore;
+import com.kaizten.opt.move.MoveRoutesSolutionMoveAfter;
+import com.kaizten.opt.move.MoveRoutesSolutionMoveBefore;
+import com.kaizten.opt.move.MoveRoutesSolutionRemove;
+import com.kaizten.opt.move.MoveRoutesSolutionSwap;
+import com.kaizten.opt.move.applier.Applier;
+import com.kaizten.opt.move.applier.MoveApplier;
+import com.kaizten.opt.move.applier.MoveApplierRoutesSolutionInsertionAfter;
+import com.kaizten.opt.move.applier.MoveApplierRoutesSolutionInsertionBefore;
+import com.kaizten.opt.move.applier.MoveApplierRoutesSolutionRemove;
+import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionInsertionAfter;
+import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionInsertionBefore;
+import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionMoveAfter;
+import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionMoveBefore;
+import com.kaizten.opt.move.generator.MoveGeneratorRoutesSolutionSwap;
+import com.kaizten.opt.move.manager.MoveManagerSequential;
 import com.kaizten.opt.builder.evaluator.EvaluatorBuilder;
 import com.kaizten.opt.evaluator.Evaluator;
 import com.kaizten.opt.solution.RoutesSolution;
 import com.kaizten.opt.solution.Solution;
-import com.kaizten.vpr.opt.io.VrpSolutionSupplier;
-import com.kaizten.vpr.opt.io.VrpSupplier;
 import com.kaizten.vrp.opt.core.Vrp;
+import com.kaizten.vrp.opt.evaluators.EvaluatorMoveAfter;
+import com.kaizten.vrp.opt.evaluators.EvaluatorMoveBefore;
+import com.kaizten.vrp.opt.evaluators.EvaluatorMoveInsertionAfter;
+import com.kaizten.vrp.opt.evaluators.EvaluatorMoveInsertionBefore;
+import com.kaizten.vrp.opt.evaluators.EvaluatorMoveRemove;
+import com.kaizten.vrp.opt.evaluators.EvaluatorMoveSwap;
 import com.kaizten.vrp.opt.evaluators.EvaluatorObjectiveFunctionDistances;
 import com.kaizten.vrp.opt.evaluators.VehicleRoutingProblemEvaluatorDistanceCplex;
-import com.kaizten.vrp.opt.solver.RandomizedBuilder;
+import com.kaizten.vrp.opt.io.VrpSupplier;
+import com.kaizten.vrp.opt.move.applier.MoveApplierRoutesSolutionMoveAfter;
+import com.kaizten.vrp.opt.move.applier.MoveApplierRoutesSolutionMoveBefore;
+import com.kaizten.vrp.opt.move.applier.MoveApplierRoutesSolutionSwap;
+import com.kaizten.vrp.opt.move.generator.MoveGeneratorRoutesSolutionRemove;
+import com.kaizten.vrp.opt.solver.RandomizedRCLBuilder;
 
 import ilog.concert.IloException;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.function.BiFunction;
 
 public class MainEvaluatorAnalyzer {
-    
-    public static final String INSTANCE_FILE = "D:\\Documentos\\Repositorios\\TFG\\vrp\\instances\\C101_100.txt";
-    public static final String SOLUTION_FILE = "D:\\Documentos\\Repositorios\\TFG\\vrp\\solutions\\Solution_C101_100.txt";
     
     @SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void main(String[] args) throws IloException {
@@ -30,7 +53,9 @@ public class MainEvaluatorAnalyzer {
         System.out.println("====================================================");
         System.out.println("Reading Vehicle Routing Problem:");
         System.out.println("====================================================");
-        Vrp optimizationProblem = new VrpSupplier().get(new File(fileProblem))
+        VrpSupplier supplierProblem =  new VrpSupplier();
+        supplierProblem.setNVehicles(10);
+        Vrp optimizationProblem = supplierProblem.get(new File(fileProblem))
                 .findFirst()
                 .get();
         System.out.println(optimizationProblem);
@@ -39,6 +64,12 @@ public class MainEvaluatorAnalyzer {
         System.out.println("====================================================");
         Evaluator<RoutesSolution<Vrp>> evaluatorCplex = EvaluatorBuilder.instance()
                 .addEvaluatorObjectiveFunction(VehicleRoutingProblemEvaluatorDistanceCplex.class)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveSwap(), 0)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveAfter(), 0)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveBefore(), 0)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveRemove(), 0)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveInsertionAfter(), 0)
+    			.addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveInsertionBefore(), 0)
                 .build();
         optimizationProblem.setEvaluator(evaluatorCplex);
         System.out.println(evaluatorCplex);
@@ -47,24 +78,23 @@ public class MainEvaluatorAnalyzer {
         System.out.println("====================================================");
         Evaluator<RoutesSolution<Vrp>> evaluator = EvaluatorBuilder.instance()
                 .addEvaluatorObjectiveFunction(EvaluatorObjectiveFunctionDistances.class)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveSwap(), 0)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveAfter(), 0)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveBefore(), 0)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveRemove(), 0)
+                .addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveInsertionAfter(), 0)
+    			.addEvaluatorObjectiveFunctionMovement(new EvaluatorMoveInsertionBefore(), 0)
                 .build();
         optimizationProblem.setEvaluator(evaluator);
         System.out.println(evaluator);
         System.out.println("====================================================");
         System.out.println("Creating Solution:");
         System.out.println("====================================================");
-        List<RoutesSolution<Vrp>> solutions = new  ArrayList<RoutesSolution<Vrp>>();
+        List<Solution<Vrp>> solutions = new  ArrayList<>();
         for(int i = 0; i < nSolutions; i++) {
-        	solutions.add(new RandomizedBuilder(optimizationProblem).run());
+        	solutions.add(new RandomizedRCLBuilder(optimizationProblem).run());
         }
          
-        Stream<RoutesSolution<Vrp>> solutionAux =  Stream.of(solutions.get(0));
-        
-        /*RoutesSolution<Vrp> solution = new VrpSolutionSupplier()
-                .get(optimizationProblem, new File(SOLUTION_FILE))
-                .findFirst()
-                .get();
-        System.out.println(solution);*/
         System.out.println("====================================================");
         System.out.println("Analyzing Solutions:");
         System.out.println("====================================================");
@@ -72,13 +102,43 @@ public class MainEvaluatorAnalyzer {
         analyzer.setOptimizationProblem(optimizationProblem);
         analyzer.addEvaluator(evaluator);
         analyzer.addEvaluator(evaluatorCplex);
-        //analyzer.setSolutions(Stream.of(solution));
-        //analyzer.setSolutions(solutions.stream());
-        //analyzer.setSolutions(solutionAux);
-        //analyzer.run();
-        for(int i = 0; i < nSolutions; i++) {
-        	analyzer.setSolutions(Stream.of(solutions.get(i)));
-        	analyzer.run();
-        }
+        analyzer.setSolutions(solutions.stream());
+        
+        /* Compare only firsts four decimals */ 
+        BiFunction<double[], double[], Boolean> comparator = (objValue1, objValue2) -> {
+        	int value1 = (int) (objValue1[0] * 10000);
+        	int value2 = (int) (objValue2[0] * 10000);
+        	return (value1 == value2); 
+        };
+        analyzer.setComparator(comparator);
+        
+        /* Compare movements */ 
+        
+        MoveManagerSequential manager = new MoveManagerSequential();
+        manager.addMoveGenerator(new MoveGeneratorRoutesSolutionSwap<RoutesSolution<Vrp>, MoveRoutesSolutionSwap>());
+		manager.addMoveGenerator(new MoveGeneratorRoutesSolutionMoveAfter<RoutesSolution<Vrp>, MoveRoutesSolutionMoveAfter>());
+		manager.addMoveGenerator(new MoveGeneratorRoutesSolutionMoveBefore<RoutesSolution<Vrp>, MoveRoutesSolutionMoveBefore>());
+		manager.addMoveGenerator(new MoveGeneratorRoutesSolutionRemove<RoutesSolution<Vrp>, MoveRoutesSolutionRemove>());
+		manager.addMoveGenerator(new MoveGeneratorRoutesSolutionInsertionAfter<RoutesSolution<Vrp>, MoveRoutesSolutionInsertionAfter>());
+		manager.addMoveGenerator(new MoveGeneratorRoutesSolutionInsertionBefore<RoutesSolution<Vrp>, MoveRoutesSolutionInsertionBefore>());
+		
+		Applier applier =  new Applier<RoutesSolution<Vrp>>();
+		MoveApplier applierSwap =  new MoveApplierRoutesSolutionSwap();
+		MoveApplier applierMoveAfter = new MoveApplierRoutesSolutionMoveAfter();
+		MoveApplier applierMoveBefore = new MoveApplierRoutesSolutionMoveBefore();
+		MoveApplier applierRemove = new MoveApplierRoutesSolutionRemove();
+		MoveApplier applierInsertionAfter = new MoveApplierRoutesSolutionInsertionAfter();
+		MoveApplier applierInsertionBefore = new MoveApplierRoutesSolutionInsertionBefore();
+		applier.addMoveApplier(applierSwap);
+		applier.addMoveApplier(applierMoveAfter);
+		applier.addMoveApplier(applierMoveBefore);
+		applier.addMoveApplier(applierRemove);
+		applier.addMoveApplier(applierInsertionAfter);
+		applier.addMoveApplier(applierInsertionBefore);
+		
+		analyzer.setMoveManager(manager);
+		analyzer.setApplier(applier);
+        
+        analyzer.run();
     }
 }
